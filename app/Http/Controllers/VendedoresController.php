4ef59d6e19830b1291212b4d96857478da109ef2
Isaac00rz <Isaac_marqz@outlook.com>
2019-05-14 19:25:45 -0500

    Cambios en seguridad

--- app/Http/Controllers/VendedoresController.php
+++ app/Http/Controllers/VendedoresController.php
@@ -4,137 +4,101 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Auth;
+
 
 class VendedoresController extends Controller
 {
-    public function index(){
-        if(Auth::check()){
-            return view('Menus/vendedoresMenu');
 
-        }else{
-            return redirect('/login');
-        }
+    public function __construct(){
+        $this->middleware('auth');
+    }
+
+    public function index(){
+        return view('Menus/vendedoresMenu');
     }
 
     public function formularioAlta(){
-        if(Auth::check()){
-            return view('Altas/altaVendedores');
-
-        }else{
-            return redirect('/login');
-        }
+        return view('Altas/altaVendedores');
     }
 
     public function altaVendedor(Request $request){
-        if(Auth::check()){
-            $array_nombre = $request->input('nombre');
-            $array_ApellidoP = $request->input('apellidoP');
-            $array_ApellidoM = $request->input('apellidoM');
-            $array_telefono = $request->input('telefono');
-            $array_email = $request->input('email');
-            $contador = 0;
-            $registros = count($array_nombre);
+        $array_nombre = $request->input('nombre');
+        $array_ApellidoP = $request->input('apellidoP');
+        $array_ApellidoM = $request->input('apellidoM');
+        $array_telefono = $request->input('telefono');
+        $array_email = $request->input('email');
+        $contador = 0;
+        $registros = count($array_nombre);
 
-            foreach($array_nombre as $i=>$t) {
-                $consulta = DB::table('vendedor')
-                ->insert(['nombre'=> $array_nombre[$i],
-                'apellidoP'=>$array_ApellidoP[$i],'apellidoM' => $array_ApellidoM[$i],
-                'telefono'=>$array_telefono[$i],'email'=>$array_email[$i]]);
+        foreach($array_nombre as $i=>$t) {
+            $consulta = DB::table('vendedor')
+            ->insert(['nombre'=> $array_nombre[$i],
+            'apellidoP'=>$array_ApellidoP[$i],'apellidoM' => $array_ApellidoM[$i],
+            'telefono'=>$array_telefono[$i],'email'=>$array_email[$i]]);
 
-                $contador++;
-            }
-            if($registros==$contador){
-                return redirect('Vendedores/Alta')->with('message', 'Datos Insertados'); ;
-            }else{
-                return redirect('Vendedores/Alta')->with('message', 'Datos No Insertados'); ;
-            }
+            $contador++;
+        }
+        if($registros==$contador){
+            return redirect('Vendedores/Alta')->with('message', 'Datos Insertados'); ;
         }else{
-            return redirect('/login');
+            return redirect('Vendedores/Alta')->with('message', 'Datos No Insertados'); ;
         }
     }
 
     public function busqueda(){
-        if(Auth::check()){
-            $consulta = DB::table('vendedor')
+        $consulta = DB::table('vendedor')
             ->select(DB::raw("id, CONCAT(nombre,' ',apellidoP,' ',apellidoM) as nombre, telefono, email")) 
             ->paginate(10);
-            return view('/Busquedas/busquedaVendedor')->with('vendedores',$consulta);
-
-        }else{
-            return redirect('/login');
-        }
+        return view('/Busquedas/busquedaVendedor')->with('vendedores',$consulta);
     }
 
     public function formularioMod($idVendedor){
-        if(Auth::check()){
-            $consulta = DB::table('vendedor')
+        $consulta = DB::table('vendedor')
             ->select('id','nombre','apellidoP','apellidoM','telefono','email') 
             ->where('id','=',$idVendedor)
             ->get();
-            return view('/Modificacion/modificarVendedor')->with('vendedores',$consulta);
-
-        }else{
-            return redirect('/login');
-        }
+        return view('/Modificacion/modificarVendedor')->with('vendedores',$consulta);
     }
 
     public function editarVendedor(Request $request){
-        if(Auth::check()){
-            $nombre = $request->input('nombre');
-            $apellidoP = $request->input('apellidoP');
-            $apellidoM = $request->input('apellidoM');
-            $telefono = $request->input('telefono');
-            $email = $request->input('email');
-            $idVendedor = $request->input('id');
+        $nombre = $request->input('nombre');
+        $apellidoP = $request->input('apellidoP');
+        $apellidoM = $request->input('apellidoM');
+        $telefono = $request->input('telefono');
+        $email = $request->input('email');
+        $idVendedor = $request->input('id');
 
-            $consulta = DB::table('vendedor')
-                ->where('id','=',$idVendedor)
-                ->update(['nombre'=> $nombre,
-                'apellidoP'=>$apellidoP,'apellidoM' => $apellidoM,
-                'telefono'=>$telefono,'email'=>$email]);
+        $consulta = DB::table('vendedor')
+            ->where('id','=',$idVendedor)
+            ->update(['nombre'=> $nombre,
+            'apellidoP'=>$apellidoP,'apellidoM' => $apellidoM,
+            'telefono'=>$telefono,'email'=>$email]);
 
-            return redirect('Vendedores/BajaMod')->with('message', 'Datos Modificados');
-        }else{
-            return redirect('/login');
-        }
-    }
+        return redirect('Vendedores/BajaMod')->with('message', 'Datos Modificados');
+}
 
     public function eliminar($idVendedor){
-        if(Auth::check()){
-            $consulta = DB::table('vendedor')
-                ->where('id','=',$idVendedor)
-                ->delete();
+        $consulta = DB::table('vendedor')
+            ->where('id','=',$idVendedor)
+            ->delete();
 
-            return redirect('Vendedores/BajaMod')->with('message', 'Datos Modificados');
-        }else{
-            return redirect('/login');
-        }
+        return redirect('Vendedores/BajaMod')->with('message', 'Datos Modificados');
     }
 
     public function buscarTodo(){
-        if(Auth::check()){
-            $consulta = DB::table('vendedor')
+        $consulta = DB::table('vendedor')
             ->select(DB::raw("id, CONCAT(nombre,' ',apellidoP,' ',apellidoM) as nombre, telefono, email")) 
             ->paginate(10);
-            return view('/Busquedas/busquedaAVendedor')->with('vendedores',$consulta);
-
-        }else{
-            return redirect('/login');
-        }
+        return view('/Busquedas/busquedaAVendedor')->with('vendedores',$consulta);
     }
 
     public function buscarNombre(Request $request){
-        if(Auth::check()){
-            $nombre = $request->input('nombre');
+        $nombre = $request->input('nombre');
 
-            $consulta = DB::table('vendedor')
+        $consulta = DB::table('vendedor')
             ->select(DB::raw("id, CONCAT(nombre,' ',apellidoP,' ',apellidoM) as nombre, telefono, email")) 
             ->where('nombre','like',"%".$nombre."%")
             ->paginate(10);
-            return view('/Busquedas/busquedaAVendedor')->with('vendedores',$consulta);
-        }else{
-            return redirect('/login');
-        }
+        return view('/Busquedas/busquedaAVendedor')->with('vendedores',$consulta);
     }
 }
